bb31313e4223447681d442fc76660e92e98781ba
Elivan Kook <elivanmetal@gmail.com>
2018-07-24 14:06:39 +0200

    10.3 create routes for orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+    # Will only receive data.
+    def create
+    end
+
+    def destroy
+    end
+end
