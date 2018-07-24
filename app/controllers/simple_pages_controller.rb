bd9f31d1a200f68ad646a199b63522e8750ed4b0
Elivan Kook <elivanmetal@gmail.com>
2018-07-24 13:47:38 +0200

    4.3 Add custom controller action

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,13 @@ class SimplePagesController < ApplicationController
 
   def contact
   end
+
+  # Adding custom controller action.
+  # The method will retrive the frist product in the products table
+  # and set it to a variable called @featured_product
+  # that we'll reference within the view - the page in no longer static.
+  def landing_page
+    @featured_product = Product.first
+  end
+
 end
