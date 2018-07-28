b9f089ee3f3ddd6b067818e65b3d895a02dca062
Elivan Kook <elivanmetal@gmail.com>
2018-07-28 17:31:43 +0200

    4.4 design the db architecture to create a model for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
     # This sets up the relationship between orders and products 
     # Rails expects to find a column in the orders db table
     # called product_id that refernces the ID of the product in the order.
+    # Each order can be assigned to one product.
     belongs_to :product
 end
 
