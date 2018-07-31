da484917aeb8f1dcafbd187e10a08e650f3913fd
Elivan Kook <elivanmetal@gmail.com>
2018-07-31 12:30:46 +0200

    4.6 Dynamic Rails Web App create simple views for controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,11 @@ class OrdersController < ApplicationController
     def index
         # To retrieve all the orders made by users so we can lay
         # them out to display
-        @orders = Order.all
+        @orders = Order.incluedes(:product).all
     end
 
     def show
+        @order = Order.find(params[:id])
     end
     # Will only receive data.
     def create
