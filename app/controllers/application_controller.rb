809d8816286426c3bcd908ea196af7790cdce92f
Elivan Kook <elivanmetal@gmail.com>
2018-08-07 15:04:26 +0200

    add JavaScript fadeout for alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    # Redirect user to home page with a message when access of
+    # another user is denied.
+    rescue_from CanCan::AccessDenied do |exception|
+        redirect_to main_app.root_url, alert: exception.message
+    end
 end
