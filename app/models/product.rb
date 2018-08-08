56ab95e9d65b3a825cc1fc43120b1979908b4644
Elivan Kook <elivanmetal@gmail.com>
2018-08-08 20:28:46 +0200

    add comment scope rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,12 @@ class Product < ApplicationRecord
     def self.search(search_term)
         Product.where("name LIKE ?", "#{search_term}%")
     end
+
     def highest_rating_comment
         comments.rating_desc.first 
+    end
+
+    def lowest_rating_comment
+        comments.rating_asc.first
     end
 end
