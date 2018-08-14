b70eda1d04fd1135399a66bfa723d447ea480dd3
Elivan Kook <elivanmetal@gmail.com>
2018-08-14 20:24:05 +0200

    6.2 unit test, add spec average_rating and validation

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Product do
+    context "when the product has comments" do
+        # Here we create a product
+        let(:product) { Product.create!(name: "race bike") }
+        # Here we create a new user 
+        #let(:user) { User.create!(name: "Jon Smith", email: "jon@gmail.com", password: "123456") }
+        # We use before instead of a let since we won't need to access these 
+        # comments directly:
+        before do
+            product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+            product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+            product.comments.create!(rating: 5, user: user, body: "Great bike!"    
+        end
+
+        it "returns the average rating of all comments" do
+            expect(product.average_rating).to eq "3"
+        end
+
+        it "is not valid without a name" do
+            expect(Product.new(description: "Nice bike")).not_to be_valid
+         end
+    end
+end
