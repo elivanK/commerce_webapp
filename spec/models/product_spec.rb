191107e953cc4c945c89c722328d1103ec4ffc74
Elivan Kook <elivanmetal@gmail.com>
2018-08-14 21:07:27 +0200

    done with the tests for average rating and validation, 0 failures

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,17 +5,17 @@ describe Product do
         # Here we create a product
         let(:product) { Product.create!(name: "race bike") }
         # Here we create a new user 
-        #let(:user) { User.create!(name: "Jon Smith", email: "jon@gmail.com", password: "123456") }
+        let(:user) { User.create!(email: "test@test.com", password: "test123") }
         # We use before instead of a let since we won't need to access these 
         # comments directly:
         before do
             product.comments.create!(rating: 1, user: user, body: "Awful bike!")
             product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-            product.comments.create!(rating: 5, user: user, body: "Great bike!"    
+            product.comments.create!(rating: 5, user: user, body: "Great bike!")    
         end
 
         it "returns the average rating of all comments" do
-            expect(product.average_rating).to eq "3"
+            expect(product.average_rating).to eq 3.0
         end
 
         it "is not valid without a name" do
