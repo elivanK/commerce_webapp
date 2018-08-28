31d6cac5221176ddf947273902ff832c6332eb5a
Elivan Kook <elivanmetal@gmail.com>
2018-08-28 19:31:46 +0200

    6.14 posting comments handled by ActiveJob'

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,6 +10,9 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     received: function(data) {
         // Called when there's incoming data on the websocket for this channel
         $(".alert.alert-info").show();
+        $('.product-reviews').prepend(data.comment);
+        $("#average-rating").attr('data-score', data.average_rating);
+        refreshRating();
     },
     //We want to access the product_id 
     //We will call this method when a user opens the page.
