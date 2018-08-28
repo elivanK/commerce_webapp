a5dc30fd3fd09aa88d9b8c92137fa809e87444fb
Elivan Kook <elivanmetal@gmail.com>
2018-08-28 18:31:36 +0200

    6.14 comment on different product pages

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,5 +10,17 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     received: function(data) {
         // Called when there's incoming data on the websocket for this channel
         $(".alert.alert-info").show();
+    },
+    //We want to access the product_id 
+    //We will call this method when a user opens the page.
+    listen_to_comments: function() {
+        return this.perform('listen', {
+            product_id: $("[data-product-id]").data("product-id")
+        });
     }
+});
+//We need to make sure the listen_to_comments is called
+//every single time the user loads a new page
+$(document).on('turbolinks:load', function() {
+    App.product.listen_to_comments();
 });
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,25 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+  
+  # we need to define the current_user property ones a user is
+  # successfully connected.
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    def disconnect
+    end
+
+    protected
+    # If the user is logged in, this method will return a user
+    # request.env['warden'] is environment variable containing info about current user
+    def find_verified_user
+      if verified_user = request.env['warden'].user
+        verified_user
+      else 
+        reject_unauthorized_connection  
+      end 
+    end
   end
 end
